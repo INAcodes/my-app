5930c6f3c6f0138efb060642d0f63223b07f7b41
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-05-09 22:55:13 +0200

    added new components in project page

---
+++ src/components/WorkCradData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/search.jpeg";
+import pro2 from "../assets/social.jpeg";
+import pro3 from "../assets/handimg.jpeg";
+
+const ProjectCardData = [
+  {
+    imgsrc: pro1,
+    title: "Ecommerce Website Design",
+    text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this“ for over 100.000 online entrepreneurs. Best-selling marketing courses",
+    view: "",
+  },
+  {
+    imgsrc: pro2,
+    title: "LMS/Course Website Design",
+    text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this“ for over 100.000 online entrepreneurs. Best-selling marketing courses",
+    view: "",
+  },
+  {
+    imgsrc: pro3,
+    title: "Rymo Ecommerce Design",
+    text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this“ for over 100.000 online entrepreneurs. Best-selling marketing courses",
+    view: "",
+  },
+];
+
+export default ProjectCardData;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
 import PricingCard from '../components/PricingCard';
-import WorkCard from '../components/WorkCard';
+import Work from '../components/Work';
 
 
 const Project = () => {
@@ -12,7 +11,7 @@ const Project = () => {
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS" text="Some of my most recent works" />
-      <WorkCard />
+      <Work />
       <PricingCard />
       <Footer />
     </div>
