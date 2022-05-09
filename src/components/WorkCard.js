7b6531f625cc773cee5433698069fb91c0a6a8ad
INAcodes <93144864+INAcodes@users.noreply.github.com>
2022-05-09 17:55:08 +0200

    added new components

---
+++ src/components/WorkCard.js
@@ -0,0 +1,13 @@
+import "./WorkCardStyles.css";
+
+import React from 'react';
+
+const WorkCard = () => {
+  return (
+    <div className="work-container">
+     <h1 className="project-heading">Projects</h1>
+    </div>
+  )
+};
+
+export default WorkCard;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
+import PricingCard from '../components/PricingCard';
+import WorkCard from '../components/WorkCard';
 
 
 const Project = () => {
@@ -9,6 +12,8 @@ const Project = () => {
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS" text="Some of my most recent works" />
+      <WorkCard />
+      <PricingCard />
       <Footer />
     </div>
   )
